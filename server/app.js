26bc8d732b25dc2a2d1fab3605575634fa26f7c9
priyanka <priyankadutta67@gmail.com>
2018-10-12 09:22:14 +0530

    edited app.js

--- server/app.js
+++ server/app.js
@@ -2,23 +2,29 @@ const express = require('express')
 const app = express()
 const activity = require('./api/activity/index');
 const bodyParser = require('body-parser');
+const logger = require('morgan')
 
-app.use(bodyParser({ urlEncoded: {extended : true }}));
+app.use(logger('dev'))
+
+
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+ });
+ 
+
+app.use(bodyParser({
+   urlEncoded: {
+       extended: true
+   }
+}));
 app.use(bodyParser.json())
 
 
 app.use('/api/v1', activity)
-
-// app.get('/', (req, res)=>{
-//     res.send({
-//         data :{
-//             "hello" : "world"
-//         }
-//     })
-// })
-
-// app.post('/', (req, res) =>{
-    
-// } )
+app.use(function (req, res, next) {
+   next();
+});
 
 app.listen(process.env.PORT || 8000, () => console.log('I am running on ', process.env.PORT))
